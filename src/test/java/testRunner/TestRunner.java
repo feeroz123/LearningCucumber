0b10e372764b5ea6d175779a42eb62cb150a0339
feeroz123 <feeroz123@gmail.com>
2024-04-08 08:49:13 +0530

    Basic Cucumber implementation

---
+++ src/test/java/testRunner/TestRunner.java
@@ -0,0 +1,13 @@
+package testRunner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions (
+    features = "Features/OrangeHRM.feature",
+    glue = "stepDefinitions"
+)
+public class TestRunner {
+}
